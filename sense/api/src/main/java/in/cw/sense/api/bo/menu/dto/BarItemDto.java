ca60bec2ff9eb462845ad567c873849d89051b03
Akash Nigam <akashnigam@whitebay.in>
2016-09-22 23:21:51 +0530

    Remove files to include get latest changes

--- sense/api/src/main/java/in/cw/sense/api/bo/menu/dto/BarItemDto.java
+++
@@ -1,4 +0,0 @@
-package in.cw.sense.api.bo.menu.dto;
-
-public class BarItemDto extends ItemDto {
-}
--- sense/api/src/main/java/in/cw/sense/api/bo/response/StatusResponse.java
+++
@@ -1,6 +0,0 @@
-package in.cw.sense.api.bo.response;
-
-@SuppressWarnings("rawtypes")
-public class StatusResponse extends GenericResponse {
-	private static final long serialVersionUID = 1L;
-}
