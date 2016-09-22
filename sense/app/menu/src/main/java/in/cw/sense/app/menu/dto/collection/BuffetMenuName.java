107a0ead1f8ebc178e322c2cc9ca5a9ab6067b55
Akash Nigam <akashnigam@whitebay.in>
2016-09-22 14:24:19 +0530

    Deleted all files

--- sense/app/menu/src/main/java/in/cw/sense/app/menu/dto/collection/BuffetMenuName.java
+++
@@ -1,7 +0,0 @@
-package in.cw.sense.app.menu.dto.collection;
-
-import org.springframework.data.mongodb.core.mapping.Document;
-
-@Document(collection = "menu_names")
-public class BuffetMenuName extends MenuNames {
-}
