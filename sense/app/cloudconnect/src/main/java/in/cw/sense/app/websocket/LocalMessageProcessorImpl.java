098df639e3cc551e8229774e22bddbf0592073c3
yogeshsadula <yogeshsadula@cantwait.in>
2016-10-01 19:40:37 +0530

    EM LS:Updating local DB syncStatus as'IN_SYNC' after cloudDB update

--- sense/app/cloudconnect/src/main/java/in/cw/sense/app/websocket/LocalMessageProcessorImpl.java
+++ sense/app/cloudconnect/src/main/java/in/cw/sense/app/websocket/LocalMessageProcessorImpl.java
@@ -5,6 +5,11 @@ import java.util.List;
 import javax.websocket.Session;
 
 import org.apache.log4j.Logger;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+
+import com.mongodb.MongoClient;
 
 import in.cw.csense.app.message.element.BillAckMessageElement;
 import in.cw.csense.app.message.element.BillDetailMessageElement;
@@ -14,6 +19,8 @@ import in.cw.csense.app.message.element.RequestMessageElement;
 import in.cw.csense.app.message.processor.MessageProcessor;
 import in.cw.csense.app.socket.SessionCollector;
 import in.cw.sense.api.bo.bill.dto.BillDto;
+import in.cw.sense.api.bo.bill.entity.BillEntity;
+import in.cw.sense.api.bo.bill.type.CloudSyncStatusType;
 import in.cw.sense.api.bo.setting.dto.CloudConnectDto;
 
 public class LocalMessageProcessorImpl implements MessageProcessor {
@@ -78,15 +85,40 @@ public class LocalMessageProcessorImpl implements MessageProcessor {
 				LOG.debug("No bills were sucessfully processed....");
 			} else {
 				for (Integer billId : success) {
-					LOG.debug("sucess bill id : " + billId);
+					LOG.debug("Updating local sync status for bill id : " + billId);
+					try {
+						updateBillSyncStatus(billId, CloudSyncStatusType.IN_SYNC);
+					} catch (Exception e) {
+						LOG.error("Exception occured while updating cloud sync status on local DB", e);
+					}
 				}
 			}
 
 			if (!failed.isEmpty()) {
 				for (Integer billId : failed) {
-					LOG.debug("Failed bill id : " + billId);
+					LOG.debug("List of bill ids which were failed in cloud sync : " + billId);
 				}
 			}
 		}
+	}
+	
+	public void updateBillSyncStatus(Integer billId, CloudSyncStatusType syncStatus) throws Exception {
+		if (billId != null && syncStatus != null) {
+			BillEntity bill = mongoTemplate().findById(billId, BillEntity.class);
+			if (bill != null) {
+				bill.setSyncStatus(syncStatus);
+				mongoTemplate().save(bill);
+			}
+		}
+	}
+	
+	//ToDo : Remove This once autowiring is done
+	public MongoDbFactory mongoDbFactory() throws Exception {
+		return new SimpleMongoDbFactory(new MongoClient("localhost", 27017), "sense-local-db");
+	}
+
+	public MongoTemplate mongoTemplate() throws Exception {
+		MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory());
+		return mongoTemplate;
 	}
 }
--- sense/app/core/src/main/java/in/cw/sense/app/job/SenseContextLoader.java
+++ sense/app/core/src/main/java/in/cw/sense/app/job/SenseContextLoader.java
@@ -2,13 +2,15 @@ package in.cw.sense.app.job;
 
 import javax.servlet.ServletContextEvent;
 
+import org.apache.log4j.Logger;
 import org.springframework.web.context.ContextLoaderListener;
 
 public class SenseContextLoader extends ContextLoaderListener {
+	private static final Logger LOG = Logger.getLogger(SenseContextLoader.class);
 
 	@Override
 	public void contextInitialized(ServletContextEvent event) {
-		System.out.println("Loading context... ");
+		LOG.debug("Loading context... ");
 		super.contextInitialized(event);
 	}
 }
